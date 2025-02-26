00f3c02e82fb960522542df8acf3b25347bccb86
ETRI우예린 <yrwoo@etri.re.kr>
2025-02-26 17:05:16 +0900

    fix: update base provider

--- src/providers/base_provider.py
+++ src/providers/base_provider.py
@@ -1,6 +1,10 @@
 from abc import ABC, abstractmethod
 
 class BaseProvider(ABC):
+    def __init__(self, collection_name: str, uid_field: str):
+        self.collection_name = collection_name
+        self.uid_field = uid_field
+        
     @abstractmethod
     def get_schema(self):
         pass
